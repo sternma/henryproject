1977af96b542c46ea512d6be8d79bd0cf6e62a7a
username0106 <lixiliang7@gmail.com>
2023-01-21 16:21:36 -0500

    added the rest of the days in posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,6 +9,11 @@ const Posts = () => {
             <h1>POSTS</h1>
             <Post name='Monday'/>
             <Post name='Tuesday'/>
+            <Post name='Wednesday'/>
+            <Post name='Thursday'/>
+            <Post name='Friday'/>
+            <Post name='Saturday'/>
+            <Post name='Sunday'/>
         </>
     )
 }
