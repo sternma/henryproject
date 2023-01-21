33c6a84a84d345f1303b52a79985f8cdd7a783be
username0106 <lixiliang7@gmail.com>
2023-01-21 16:29:16 -0500

    changed the alignItems and justifyContent to center

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
 import memories from './images/memories.png';
 import useStyles from './styles';
 
@@ -16,12 +15,9 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify= "space-between" alignItems = "stretch" spacing = {3}>
+                    <Grid container justifyContent = "center" alignItems = "center" spacing = {3}>
                         <Grid item xs = {12} sm = {7}>
                             <Posts />
-                        </Grid>
-                        <Grid item xs = {12} sm = {4}>
-                            <Form />
                         </Grid>
                     </Grid>
                 </Container>
