3108accf2bb2452ac094975171fbce05e8ddbcab
username0106 <lixiliang7@gmail.com>
2023-01-29 17:49:16 -0500

    tried to fix the days of the week

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
-import Posts from './components/Posts/Posts';
+import Post from './components/Posts/Post/Post';
 import memories from './images/memories.png';
 import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
     return (
-        <Container maxidth = "lg">
+        <Container maxWidth = "lg">
             <AppBar className={classes.appBar} position = "static" color = "inherit">
                 <img className={classes.image} src= {memories} alt = "Week Schedule" height = "60" />
                 <Typography className={classes.heading} varaint = "h2" align = "center">Week Schedule</Typography>
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justifyContent = "center" alignItems = "center" spacing = {3}>
+                    <Grid container justifyContent = "space-between" alignItems = "stretch" spacing = {3}>
                         <Grid item xs = {12} sm = {7}>
-                            <Posts />
+                            <Post name='Monday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Tuesday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Wednesday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Thursday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Friday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Saturday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Sunday'/>
                         </Grid>
                     </Grid>
                 </Container>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Post from './Post/Post';
 import useStyles from './styles';
+import { Container, Grow, Grid } from '@material-ui/core';
+
 
 const Posts = () => {
     const classes = useStyles();
@@ -14,6 +16,35 @@ const Posts = () => {
             <Post name='Friday'/>
             <Post name='Saturday'/>
             <Post name='Sunday'/>
+            
+            {/* <Grow in>
+                <Container>
+                    <Grid container justifyContent = "center" alignItems = "stretch" spacing = {3} direction = "row">
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Monday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                           <Post name='Tuesday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Wednesday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Thursday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Friday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Saturday'/>
+                        </Grid>
+                        <Grid item xs = {12} sm = {7}>
+                            <Post name='Sunday'/>
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow> */}
         </>
     )
 }
