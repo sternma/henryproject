f3942b06712a0d3d4a172904e6da641273c416a7
Matthew Stern <matthewstern@Matthews-MBP.lan>
2023-01-31 20:00:40 -0500

    added sample task text

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,7 +4,16 @@ import useStyles from './styles';
 const Post = ({ name }) => {
     const classes = useStyles();
     return(
+        <>
         <h1>{name}</h1>
+        <h3>Task Name:</h3>
+        <p>Sample Task</p>
+        <h3>Task Details:</h3>
+        <p>-Do this</p>
+        <p>-Do that</p>
+        <h3>Complete?</h3>
+        <p>No</p>
+        </>   
     )
 }
 
